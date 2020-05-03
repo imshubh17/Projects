fe7d99b894cb2104357846048f4dbb2fcb3feb1d
imshubh17 <sss.1771997@gmail.com>
2020-05-03 21:38:28 +0530

    push todo app

---
+++ todoapp/README.md
@@ -0,0 +1,73 @@
+TodoApp
+-----
+### Introduction
+Todo app use to create task in different categoery and delete after completion.
+
+### Overview
+
+you should have a fully functioning site that is at least capable of doing the following, if not more, using a PostgreSQL database:
+
+* creating new Task.
+* checklist to complete.
+* delete after completion.
+
+### Tech Stack
+
+Our tech stack will include:
+
+* **SQLAlchemy ORM** to be our ORM library of choice
+* **PostgreSQL** as our database of choice
+* **Python3** and **Flask** as our server language and server framework
+* **Flask-Migrate** for creating and running schema migrations
+* **HTML**, **CSS**, and **Javascript** with [Bootstrap 3](https://getbootstrap.com/docs/3.4/customize/) for our website's frontend
+
+ ```sh
+  ├── README.md
+  ├── app.py *** the main driver of the app. Includes your SQLAlchemy models.
+                    "python app.py" to run after installing dependences
+  ├── requirements.txt *** The dependencies we need to install with "pip3 install -r requirements.txt"
+  ├── static
+  │   ├── css 
+  └── templates
+      ├── index.html
+  ```
+### Setup
+1. Install the dependencies:
+  ```
+  $ pip install -r requirements.txt
+  ```
+2. setup your database:
+  ```
+  app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://username:password@localhost:5432/databaseName'
+  ```
+  in app.py file.
+
+3. Run the development server:
+
+  ```
+  You can then generate an initial migration
+    $ flask db migrate -m "Initial migration."
+
+  Then you can apply the migration to the database:
+    $ flask db upgrade
+  ```
+4. insert data into todolist table.
+
+5. migrate && upgrade database:
+  ```
+  $ python app.py
+  ```
+![Home page](https://github.com/imshubh17/Projects/blob/master/images/todoapp.PNG?raw=true "TodoApp")
+
+#### Extra activity
+Install Flask-Migrate with pip:
+pip install Flask-Migrate
+
+With the above application you can create a migration repository with the following command:
+$ flask db init
+
+You can then generate an initial migration
+$ flask db migrate -m "Initial migration."
+
+Then you can apply the migration to the database:
+$ flask db upgrade
---
+++ todoapp/app.py
@@ -0,0 +1,94 @@
+from flask import Flask, render_template ,redirect, request,jsonify, url_for, abort
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+import sys
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:123@localhost:5432/todoapp'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+class Todo(db.Model):
+  __tablename__ = 'todo'
+  id = db.Column(db.Integer, primary_key=True)
+  description = db.Column(db.String(), nullable=False)
+  completed = db.Column(db.Boolean, nullable=True, default=False)
+  list_id = db.Column(db.Integer, db.ForeignKey('todolist.id'),nullable=False)
+
+  def __repr__(self):
+      return f'<Todo ID: {self.id}, description: {self.description}>'
+
+class TodoList(db.Model):
+    __tablename__= "todolist"
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(), nullable=False)
+    todos = db.relationship('Todo', backref='list',lazy=True)
+
+
+@app.route('/todos/create', methods=['POST'])
+def create_todo():
+
+    error = False
+    body = {}
+    try:
+        description = request.get_json()['description']
+        list_id = request.get_json()['list_id']
+        todo = Todo(description=description)
+        active_list = TodoList.query.get(list_id)
+        todo.list = active_list
+        db.session.add(todo)
+        db.session.commit()
+        body['description'] = todo.description
+    except:
+        error = True
+        db.session.rollback()
+        print(sys.exc_info())
+    finally:
+        db.session.close()
+    if not error:
+        return jsonify(body)
+    else:
+        abort(500)
+
+@app.route('/todos/<todo_id>', methods=['DELETE'])
+def delete_todo(todo_id):    
+    try:
+        Todo.query.filter_by(id=todo_id).delete()
+        db.session.commit()
+    except:
+        db.session.rollback()
+    finally:
+        db.session.close()
+    return jsonify({ 'success': True })
+
+@app.route('/todos/<todo_id>/set-completed', methods=['POST'])
+def set_completed_todo(todo_id):
+    try:
+        completed = request.get_json()['completed']
+        print('completed', completed)
+        todo = Todo.query.get(todo_id)
+        todo.completed = completed
+        db.session.commit()
+    except:
+        db.session.rollback()
+    finally:
+        db.session.close()
+    return redirect(url_for('index'))
+
+@app.route('/lists/<list_id>')
+def get_list_todos(list_id):
+    return render_template('index.html',
+    lists=TodoList.query.all(),
+    active_list=TodoList.query.get(list_id),
+    todos=Todo.query.filter_by(list_id=list_id).order_by('id').all()
+    )
+
+@app.route('/')
+def index():
+    return redirect(url_for('get_list_todos', list_id=1))
+
+
+if __name__ == '__main__':
+  app.run(debug=True)
